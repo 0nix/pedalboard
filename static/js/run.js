421d6eb9d0584f412a7b9ff28da12f6b9f6faf03
0nix <boundlessimagination@gmail.com>
2015-04-21 18:49:11 -0500

    Server Sync w Editor

--- static/js/run.js
+++ static/js/run.js
@@ -1,15 +1,62 @@
-var mat = function(){
-	 $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: true, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on click
-      alignment: 'right', // Aligns dropdown to left or right edge (works with constrain_width)
-      gutter: 0, // Spacing from edge
-      belowOrigin: false // Displays dropdown below the button
-    });
+var bone = {};
+var model = function(){
+	bone.Model = Backbone.Model.extend({
+		defaults: function(){
+			return {
+				serializedView:"",
+				linkedList: [],
+				relayAddress: ""
+			}
+		},
+		pollServer:function(){
+
+		},
+		relayServer:function(){
+			var pkg = {
+				serial: this.get("serializedView")
+			}
+			var add = this.get("relayAddress");
+			$.ajax({
+				method: "PUT",
+				data: pkg,
+				url: add
+			}).then(function(data,status,xhr){
+				console.log("RELAY DONE");
+			});
+
+		},
+		serializeView: function(view){
+			var s = JSON.stringify(view);
+			this.set("serializedView",window.btoa(s));
+			this.relayServer();
+		},
+		deserializeView:function(){
+			return JSON.parse(window.atob(this.get("serializedView")));
+		}
+	});
+	bone.BoneModel = new bone.Model;
+	bone.View = Backbone.View.extend({
+		initialize:function(){
+			this.listeners();
+			bone.BoneModel.set("relayAddress", 
+				"./api/" + window.location.pathname.split("/editor/")[1]);
+			//this.listenTo(app.AppModel,"serializedView")
+		},
+		listeners:function(){
+			 $('.dropdown-button').dropdown({
+			      inDuration: 300,
+			      outDuration: 225,
+			      constrain_width: true, // Does not change width of dropdown to that of the activator
+			      hover: false, // Activate on click
+			      alignment: 'right', // Aligns dropdown to left or right edge (works with constrain_width)
+			      gutter: 0, // Spacing from edge
+			      belowOrigin: false // Displays dropdown below the button
+    		});
+		}
+	});
+	bone.BoneView = new bone.View;
 }
-var run = function(){
+var view = function(){
 	var graph = new joint.dia.Graph;
 	var toSet;
 	var elementPrimed = false;
@@ -52,10 +99,7 @@ var run = function(){
 			r.position(x,y);
 			graph.addCells([r]);
 			elementPrimed = false;
-			var str = JSON.stringify(graph.toJSON());
-			var b64 = window.btoa(str);
-			console.log(b64);
-			console.log(window.atob(b64));
+			bone.BoneModel.serializeView(graph.toJSON());
 		}
 	});
 	/*paper.on("cell:pointerclick",function(ev,x,y){
@@ -90,5 +134,5 @@ var run = function(){
 		toSet = ev.currentTarget.id;
 	});
 }
-mat();
-run();
+model();
+view();
