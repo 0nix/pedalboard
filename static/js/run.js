328b986641a6e9fe496a6d1ad958184c40a46d0f
0nix <boundlessimagination@gmail.com>
2015-04-25 22:16:26 -0500

    GUI Overhaul

--- static/js/run.js
+++ static/js/run.js
@@ -1,12 +1,23 @@
 var bone = {};
+var viewK = {};
 var model = function(){
 	bone.Model = Backbone.Model.extend({
 		defaults: function(){
 			return {
+				state: 0,
 				serializedView:"",
 				linkedList: [],
 				relayAddress: "",
 				prevExist: false
+			}
+		},
+		genDatObj:function(){
+			var that = this;
+			return { 
+				pedal: "",
+				pedalAdd:function() {
+					that.trigger("change:addPedal");
+				}
 			}
 		},
 		relayServer:function(){
@@ -35,76 +46,75 @@ var model = function(){
 	});
 	bone.BoneModel = new bone.Model;
 	bone.View = Backbone.View.extend({
-		initialize:function(){
-			this.listeners();
+		initialize:function(){;
 			bone.BoneModel.set("relayAddress", 
 				"./api/" + window.location.pathname.split("/editor/")[1]);
+			this.generalGUI();
+			this.viewKernel();
+			this.listenTo(bone.BoneModel,"change:addPedal", viewK.addaPedal)
 		},
-		listeners:function(){
-			 $('.dropdown-button').dropdown({
-			      inDuration: 300,
-			      outDuration: 225,
-			      constrain_width: true, // Does not change width of dropdown to that of the activator
-			      hover: false, // Activate on click
-			      alignment: 'right', // Aligns dropdown to left or right edge (works with constrain_width)
-			      gutter: 0, // Spacing from edge
-			      belowOrigin: false // Displays dropdown below the button
-    		});
+		viewKernel:function(){
+			viewK.graph = new joint.dia.Graph;
+			viewK.toSet;
+			viewK.elementPrimed = false;
+			viewK.linkPrimed = false;
+			viewK.toLink;
+			viewK.paper = new joint.dia.Paper({
+				el: $("#canvas"),
+				model: viewK.graph,
+				gridSize: 1
+			});
+			viewK.component = function(w, h, imageDir){
+			return new joint.shapes.devs.Atomic({
+				size:{ width: w || 100, height: h || 168},
+				inPorts:["In"],
+				outPorts:["L","R"],
+				attrs:{
+					image:{
+						"xlink:href": imageDir || "../gameboy.png",
+						//"xlink:href": imageDir || "http://placehold.it/100x168",
+						width: w || 100,
+						height: h || 168
+					},
+					rect: { fill: 'none' }, 
+					text: { text: 'test', display:"none" }
+						}
+				});
+			};
+			viewK.addaPedal = function(){
+				var r = viewK.component();
+				r.position(10,10);
+				viewK.graph.addCells([r]);
+				bone.BoneModel.serializeView(viewK.graph.toJSON());
+			}
+			viewK.link = function(source, inport, dest, outport){
+				var link = new joint.shapes.devs.Link({
+					source: {id:source.id, selector: source.getPortSelector(inport)},
+					target: {id:dest.id, selector: dest.getPortSelector(outport)}
+				});
+				link.addTo(viewK.graph).reparent();
+			};
+		},
+		generalGUI: function(){
+			viewK.guiTool = bone.BoneModel.genDatObj();
+			viewK.gui = new dat.GUI();
+			viewK.f1 = viewK.gui.addFolder('Add a Pedal');
+			viewK.f1.add(viewK.guiTool, 'pedal', [ 'Distorsion', 'Delay', 'Flanger' ] );
+			viewK.f1.add(viewK.guiTool, 'pedalAdd' );
+			viewK.f1.open();
+		},
+		flushGUI:function(){
+			$('.dg.ac').remove();
+			delete viewK.guiTool;
+			delete viewK.gui;
+			delete viewK.f1;
+			delete viewK.f2;
 		}
 	});
 	bone.BoneView = new bone.View;
 	return true;
-	//resolve(true);
 }
 var view = function(){
-	var graph = new joint.dia.Graph;
-	var toSet;
-	var elementPrimed = false;
-	var linkPrimed = false;
-	var toLink;
-	var paper = new joint.dia.Paper({
-		el: $("#canvas"),
-		model: graph,
-		gridSize: 1
-	});
-	if(bone.BoneModel.get("prevExist")){
-		graph.fromJSON(bone.BoneModel.deserializeView());
-	}
-	var component = function(w, h, imageDir){
-		return new joint.shapes.devs.Atomic({
-			size:{ width: w || 100, height: h || 168},
-			inPorts:["In"],
-			outPorts:["L","R"],
-			attrs:{
-				image:{
-					"xlink:href": imageDir || "../gameboy.png",
-					//"xlink:href": imageDir || "http://placehold.it/100x168",
-					width: w || 100,
-					height: h || 168
-				},
-				rect: { fill: 'none' }, 
-				text: { text: 'test', display:"none" }
-			}
-		});
-	}
-	var link = function(source, inport, dest, outport){
-		var link = new joint.shapes.devs.Link({
-			source: {id:source.id, selector: source.getPortSelector(inport)},
-			target: {id:dest.id, selector: dest.getPortSelector(outport)}
-		});
-		link.addTo(graph).reparent();
-	};
-	paper.on("blank:pointerdown",function(ev,x,y){
-		$("#canvas").addClass("unselected");
-		$("#canvas").removeClass("toSelect");
-		if(elementPrimed){
-			var r = component();
-			r.position(x,y);
-			graph.addCells([r]);
-			elementPrimed = false;
-			bone.BoneModel.serializeView(graph.toJSON());
-		}
-	});
 	/*paper.on("cell:pointerclick",function(ev,x,y){
 		if(!linkPrimed){
 			//console.log(ev.model.id);
@@ -128,14 +138,15 @@ var view = function(){
 		}
 
 	});*/
-	$(".menu-item").on("click",function(ev){
+	/*$(".menu-item").on("click",function(ev){
 		ev.preventDefault();
 		$("#canvas").addClass("toSelect");
 		$("#canvas").removeClass("unselected");
 		toast('Click on the workspace to add your element', 3500);
 		elementPrimed = true;
 		toSet = ev.currentTarget.id;
-	});
+	});*/
+	//DAT GUI
 }
 var promise = new Promise(function(resolve, reject) {
     model();
@@ -152,5 +163,7 @@ var promise = new Promise(function(resolve, reject) {
 	});
 });
 promise.then(function(result) {
-	view();
+	if(bone.BoneModel.get("prevExist")){
+		viewK.graph.fromJSON(bone.BoneModel.deserializeView());
+	}
 });
